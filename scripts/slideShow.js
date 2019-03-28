ed2fcf2e3f84248fba4e4debf23f4293ee8da024
Aleksandra Jankowska <janka303@newschool.edu>
2019-03-28 13:43:20 -0400

    code commenting

--- scripts/slideShow.js
+++ scripts/slideShow.js
@@ -1,9 +1,13 @@
 // https://www.sitepoint.com/making-simple-image-slider-html-css-jquery/
 
 var currentIndex = 0,
+  //all items stored in the container div (have to make a div for each picture)
   items = $('#container div'),
+  //length of the container div (amt of items within)
   itemAmount = items.length;
 
+//this function cycles the items and allows for them to be stacked. var item is equal to current Index
+//items are hidden unless it is the current Index
 function cycleItems() {
   var item = $('#container div').eq(currentIndex);
   items.hide();
@@ -11,6 +15,7 @@ function cycleItems() {
   $("#pageNumber").html("Page Number: " + currentIndex);
 }
 
+//auto slide allows for cycle items function to go on automatically every 3 seconds
 var autoSlide = setInterval(function() {
   currentIndex += 1;
   if (currentIndex > itemAmount - 1) {
@@ -19,15 +24,19 @@ var autoSlide = setInterval(function() {
   cycleItems();
 }, 3000);
 
+//clear interval cancels auto cycle
+//next click allows for user to switch slides by themselves
 $('#next').click(function() {
   clearInterval(autoSlide);
   currentIndex += 1;
+  //if index is on the last slide then show first slide
   if (currentIndex > itemAmount - 1) {
     currentIndex = 0;
   }
   cycleItems();
 });
 
+//prev function allows to go back
 $('#prev').click(function() {
   clearInterval(autoSlide);
   currentIndex -= 1;
