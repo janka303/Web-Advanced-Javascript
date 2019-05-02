a81314e6fce2b9d7c06a8350089c2b1f846ab57c
Aleksandra Jankowska <janka303@newschool.edu>
2019-05-01 23:12:32 -0400

    work

--- scripts/final.js
+++ scripts/final.js
@@ -1,4 +1,7 @@
 let mic;
+var music = document.getElementById("mp3");
+var bubbles = 12;
+
 // var fft;
 // var smoothing = 0.8;
 //https://github.com/therewasaguy/p5-music-viz/blob/master/demos/05a_fft_particle_system/sketch.js
@@ -7,17 +10,25 @@ let mic;
 //https://p5js.org/examples/sound-oscillator-frequency.html
 
 //https://github.com/processing/p5.js-sound/issues/274 fixes audiocontext issue
+
 $(function() { 
   $("#play").click(function(){
-    getAudioContext().resume(); 
+    getAudioContext().resume();
+    playAudio();
+    $("#intro").hide();
+  
   });
   $("#pause").click(function(){
     getAudioContext().suspend();
+    pauseAudio();
   });
 });
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  var cnv = createCanvas(windowWidth, windowHeight);
+  cnv.position(0, 0);
+  cnv.style('z-index','-2');
+  getAudioContext().suspend();
 
   // Create an Audio input
   mic = new p5.AudioIn();
@@ -34,7 +45,7 @@ function draw() {
   background('#c7fcf5');
 
   // Get the overall volume (between 0 and 1.0)
-  let vol = mic.getLevel()*1000;
+  let vol = mic.getLevel()*3000;
   console.log(vol);
 
   // let spectrum = fft.analyze();
@@ -47,3 +58,14 @@ function draw() {
   // Draw an ellipse with height based on volume
   ellipse(width / 2, height/2, vol+500, vol+500);
 }
+
+// https://www.w3schools.com/jsref/met_audio_play.asp
+
+function playAudio(){
+  music.play();
+}
+
+function pauseAudio(){
+  music.pause();
+}
+
